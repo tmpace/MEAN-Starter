dbd521a9752ee24d53ee85a27ee7c6444181d297
tmpace <tmpace11@gmail.com>
2016-06-01 12:32:21 -0700

    (chore) Add comments to server.js

--- app/server.js
+++ app/server.js
@@ -1,8 +1,11 @@
+// Import express
 var express = require('express');
 
+// Create express instance & declare port
 var app = express();
 var port = process.env.PORT || 1337;
 
+// Start server
 app.listen(port, function() {
   console.log('App running on port ' + port);
 });
