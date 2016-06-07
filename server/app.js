a619e13c2d212c8134d126f00c60ad48bdaa271f
Trevor Pace <tmpace11@gmail.com>
2016-06-07 10:25:58 -0500

    (feat) Add Angular

---
+++ server/app.js
@@ -0,0 +1,20 @@
+// Import express
+var express = require('express');
+
+// Create express instance & declare port
+var app = express();
+var port = process.env.PORT || 1337;
+
+// Connect to DB
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/starter'); // Replace 'starter' with your DB name
+
+// Middleware
+app.use(express.static(__dirname + '/../client'));
+
+// Start server
+app.listen(port, function() {
+  console.log('App running on port ' + port);
+});
+
+module.exports = app;
